ba849c91a80eb27a561eeffdd2cc88b5dc167319
lokijumbo <logeswarankarthikeyan@gmail.com>
2025-05-07 14:16:39 +0530

    new push4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
